1a5dd7403c596f7dfafe5d0cd5fd217fad3fa7ee
saarcoder <spammuelltopf@gmail.com>
2021-11-08 15:58:34 +0100

    submit form with js

--- src/routes/kontakt.svelte
+++ src/routes/kontakt.svelte
@@ -45,6 +45,50 @@
         zoom: pageDetails.map.zoom,
       });
     }
+
+    var formEl = document.getElementById("contact-form");
+
+    // Helper function to get form data in the supported format
+    function getFormDataString(formEl) {
+      var formData = new FormData(formEl),
+        data = [];
+
+      for (var keyValue of formData) {
+        data.push(
+          encodeURIComponent(keyValue[0]) +
+            "=" +
+            encodeURIComponent(keyValue[1])
+        );
+      }
+
+      return data.join("&");
+    }
+
+    const handleSubmit = () => {
+      if (grecaptcha) {
+        var recaptchaResponse = grecaptcha.getResponse();
+        if (!recaptchaResponse) {
+          // reCAPTCHA not clicked yet
+          return false;
+        }
+      }
+
+      var request = new XMLHttpRequest();
+
+      request.addEventListener("load", function () {
+        if (request.status === 302) {
+          // CloudCannon redirects on success
+          // It worked
+        }
+      });
+
+      request.open(formEl.method, formEl.action);
+      request.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded"
+      );
+      request.send(getFormDataString(formEl));
+    };
   });
 
   onDestroy(async () => {
@@ -56,9 +100,11 @@
   <section class="diagonal">
     <div class="container">
       <form
+        on:submit|preventDefault={handleSubmit}
         action={`${siteData.baseurl}/contact-success`}
         method="post"
         class="contact-form"
+        id="contact-form"
       >
         <input type="hidden" name="_to" value={company.contact_email_address} />
         <input type="text" name="_gotcha" />
